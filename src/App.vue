4c49f1fb098abcad48494d211ada3753b5d2ee56
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2024-01-22 17:29:42 +0100

    Inizio

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  data() {
+    return { count: 0 }
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
 </script>
 
 <template>
-
+    <div>
+        <AppHeader />
+        <AppMain />
+        <AppFooter />
+    </div>
 </template>
 
 <style scoped>
