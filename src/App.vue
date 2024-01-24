d1a5574f8730d3c3a9fbdd149a70b88a5dc1e573
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2024-01-24 15:40:31 +0100

    Header

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,107 @@ import AppFooter from './components/AppFooter.vue';
 
 export default {
   data() {
-    return { count: 0 }
+    return {
+
+     }
   },
   components: {
     AppHeader,
     AppMain,
     AppFooter
-  }
+  },
 }
 </script>
 
 <template>
-    <div>
-        <AppHeader />
-        <AppMain />
-        <AppFooter />
-    </div>
+    <header>
+        <div>
+            <img src="../public/img/dc-logo.png" alt="DC-Logo">
+        </div>
+        <ul>
+            <li>
+                <a href="#">
+                    CHARACTERS
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    COMICS
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    MOVIES
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    TV
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    GAMES
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    COLLECTIBLES
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    VIDEOS
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    FANS
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    NEWS
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    SHOP
+                </a>
+            </li>
+        </ul>
+    </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+header {
+    display: flex;
+    justify-content: space-between;
+    padding: 5px;
 
+
+    > ul {
+        display: flex;
+        align-items: center;
+        list-style: none;
+
+        > li {
+            padding: 5px;
+            margin-left: 8px;
+            margin-right: 8px;
+
+            > a {
+                color: black;
+                font-weight: bold;
+                text-decoration: none;
+
+                &:hover {
+                    color: #0C7CEC;
+                }
+            }
+        }
+
+            
+    }
+}
 </style>
