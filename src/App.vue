839720ed084d1d340009206460a5e85350782f3e
GiorgiaQuibanLagrisola <lagrisola.giorgia@gmail.com>
2024-01-24 16:55:42 +0100

    Footer

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
 <template>
     <header>
         <div>
-            <img src="../public/img/dc-logo.png" alt="DC-Logo">
+            <img src="/img/dc-logo.png" alt="DC-Logo">
         </div>
         <ul>
             <li>
@@ -29,7 +29,7 @@ export default {
                 </a>
             </li>
             <li>
-                <a href="#">
+                <a class="active" href="#">
                     COMICS
                 </a>
             </li>
@@ -75,6 +75,209 @@ export default {
             </li>
         </ul>
     </header>
+
+    <main>
+        <div class="comics-container">
+        -> Content goes here 
+        </div>
+    </main>
+
+    <footer>
+        <div class="links-container">
+            <div>
+                <div class="buy-comics">
+                    <img src="/img/buy-comics-digital-comics.png" alt="">
+                </div>
+                
+                <a href="#">
+                    DIGITAL COMICS
+                </a>
+            </div>
+            <div>
+                <div class="buy-comics">
+                    <img src="/img/buy-comics-merchandise.png" alt="">
+                </div>
+                
+                <a href="#">
+                    DC MERCHANDISE
+                </a>
+            </div>
+            <div>
+                <div class="buy-comics">
+                    <img src="/img/buy-comics-subscriptions.png" alt="">
+                </div>
+                
+                <a href="#">
+                    SUBSCRIPTIONS
+                </a>
+            </div>
+            <div>
+                <div class="buy-comics">
+                    <img src="/img/buy-comics-shop-locator.png" alt="">
+                </div>
+                
+                <a href="#">
+                    COMIC SHOP LOCATOR
+                </a>
+            </div>
+            <div>
+                <div class="buy-comics">
+                    <img src="/img/buy-dc-power-visa.svg" alt="">
+                </div>
+                
+                <a href="#">
+                    DC POWER VISA
+                </a>
+            </div>
+        </div>
+        <div class="main-footer">
+            <div class="columns-links-container">
+                <div>
+                    <h4>
+                        DC COMICS 
+                    </h4>
+                    <ul>
+                        <li>
+                            <a href="#">
+                                Characters
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Comics 
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Movies 
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                TV
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Games
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Videos
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                News
+                            </a>                            
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h4>
+                        DC 
+                    </h4>
+                    <ul>                        
+                        <li>
+                            <a href="#">
+                                Terms Of Use
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Privacy policy (New)
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Ad Choices
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Advertising
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Jobs
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Subscriptions 
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Talent Workshops
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                CPSC Certificates
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Ratings
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Shop Help
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                Contact Us
+                            </a>                            
+                        </li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h4>
+                        SITES
+                    </h4>
+
+                    <ul>                        
+                        <li>
+                            <a href="#">
+                                DC
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                MAD Magazine
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Kids
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Universe
+                            </a>                            
+                        </li>
+                        <li>
+                            <a href="#">
+                                DC Power Visa
+                            </a>                            
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+            <div class="footer-logo-container">
+                <img src="../public/img/dc-logo-bg.png" alt="">
+            </div>
+        </div>
+    </footer>
 </template>
 
 <style lang="scss" scoped>
@@ -99,7 +302,8 @@ header {
                 font-weight: bold;
                 text-decoration: none;
 
-                &:hover {
+                &:hover,
+                &.active {
                     color: #0C7CEC;
                 }
             }
@@ -107,5 +311,41 @@ header {
 
             
     }
+}
+
+main {
+    .comics-container {
+        color: white;
+        background-color: black;
+        padding: 50px 20px;
+        font-weight: bold;
+    }
+}
+
+.links-container {
+    display: flex;
+    justify-content: space-between;
+    background-color: #0C7CEC;
+    color: white;
+    text-decoration: none;
+}
+.buy-comics {
+
+}
+
+.main-footer {
+    min-height: 400px;
+    background-image: url('../public/img/footer-bg.jpg');
+    display: flex;
+    justify-content: space-between;
+    
+}
+
+.footer-logo-container {
+    width: calc(100% / 2);
+}
+
+.columns-links-container {
+    display: flex;
 }
 </style>
